d7da0b1dfebc3e0a605674570d4c24ec9cfd52c1
Joan Marcos <joanmrc96@gmail.com>
2019-07-23 23:58:31 -0300

    Adicionado JWT

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', 'AuthController@register')->name('register.jwt');
+
+Route::post('/login', 'AuthController@login')->name('login.jwt');
+
+Route::post('/logout', 'AuthController@logout')->name('logout.jwt');
